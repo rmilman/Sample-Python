98c42b8b7b119fca90eb7e874ac82137513ced48
rmilman <beccamilman@airbears2-10-142-54-215.airbears2.1918.berkeley.edu>
2015-08-26 15:52:16 -0700

    A lab for sets
    A more basic example of code

---
+++ lab08.py
@@ -0,0 +1,109 @@
+## Linked Lists and Sets ##
+
+# Linked Lists
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1, Link(2, Link(3, Link(4))))
+    >>> len(s)
+    4
+    >>> s[2]
+    3
+    >>> s
+    Link(1, Link(2, Link(3, Link(4))))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __getitem__(self, i):
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
+
+    def __len__(self):
+        return 1 + len(self.rest)
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(repr(self.first), rest_str)
+
+def slice_link(link, start, end):
+    """Slices a Link from start to end (as with a normal Python list).
+
+    >>> link = Link(3, Link(1, Link(4, Link(1, Link(5, Link(9))))))
+    >>> slice_link(link, 1, 4)
+    Link(1, Link(4, Link(1)))
+    """
+    if start>=end:
+        return Link.empty
+    else:
+        sliced = Link(link[start])
+        sliced.rest = slice_link(link.rest, start, end - 1)
+    return sliced
+
+
+# Sets
+
+def union(s1, s2):
+    """Returns the union of two sets.
+
+    >>> r = {0, 6, 6}
+    >>> s = {1, 2, 3, 4}
+    >>> t = union(s, {1, 6})
+    >>> t
+    {1, 2, 3, 4, 6}
+    >>> union(r, t)
+    {0, 1, 2, 3, 4, 6}
+    """
+    union=s1
+    [union.add(i) for i in s2 if i not in s1]
+    return union
+
+def intersection(s1, s2):
+    """Returns the intersection of two sets.
+
+    >>> r = {0, 1, 4, 0}
+    >>> s = {1, 2, 3, 4}
+    >>> t = intersection(s, {3, 4, 2})
+    >>> t
+    {2, 3, 4}
+    >>> intersection(r, t)
+    {4}
+    """
+    intersection=set()
+    [intersection.add(i) for i in s1 if i in s2]
+    return intersection
+
+def extra_elem(a,b):
+    """B contains every element in A, and has one additional member, find
+    the additional member.
+
+    >>> extra_elem(['dog', 'cat', 'monkey'],  ['dog',  'cat',  'monkey',  'giraffe'])
+    'giraffe'
+    >>> extra_elem([1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
+    6
+    """
+    return list(set(b)-set(a))[0]
+
+def find_duplicates(lst):
+    """Returns True if lst has any duplicates and False if it does not.
+
+    >>> find_duplicates([1, 2, 3, 4, 5])
+    False
+    >>> find_duplicates([1, 2, 3, 4, 2])
+    True
+    """
+    return len(lst)>len(set(lst))
+
+    
+
+
